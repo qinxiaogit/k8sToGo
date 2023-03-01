fa951382ffd5bc56deac27dc568dc4bf26e24b54
owlet <qinxiao@zhicloud.com>
2023-03-01 14:33:36 +0800

    -

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/qinxiaogit/k8sToGo
 go 1.13
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.28.1
 )
---
+++ helloworld/cmd/client.go
@@ -0,0 +1,43 @@
+package main
+
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/qinxiaogit/k8sToGo/helloworld/proto"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("hello service: %s", r.GetMessage())
+}
+
